20765f7418811421686b964e7a0a1bdf3911d9de
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-23 21:34:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
 
+class CashRegister
+    attr_accessor :total, :items,:item, :quantities, :price
+  def initialize(percentage = 0)
+     @percentage = percentage
+     @total = 0
+     @items = ""
+  end
+  def discount
+    @percentage
+  end
+  def add_item(item, price, quantities = 1)
+    items = self
+    items.item = item
+    items.price = price
+    items.quantities = quantities
+    items
+    quantities.times{|i| @items += " #{item}"
+  }
+    @total += price * quantities
+   end
+  def apply_discount
+  discount_total = total * discount / 100
+    @total -= discount_total
+    if discount == 0
+     "There is no discount to apply."
+    else
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+  def items
+    @items.split(" ")
+  end
+  def void_last_transaction
+  @total -= self.price * self.quantities
+  end
+end
